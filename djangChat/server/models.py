34fcb9f3f3f201d692b34040ecbf01006126211f
Malhar Patel <malhar@ualberta.ca>
2023-09-03 05:50:33 -0600

    imagefield to channel and valditor and pillow

--- djangChat/server/models.py
+++ djangChat/server/models.py
@@ -3,7 +3,17 @@ from django.db import models
 from django.dispatch import receiver
 from django.shortcuts import get_object_or_404
 
+from .validators import validate_icon_image_size, validate_image_file_exstension
+
 # foreign key is a one to many relationship and goes to model with "many"
+
+
+def server_icon_path(instance, filename):
+    return f"server/{instance.id}/server_icon/{filename}"
+
+
+def server_banner_path(instance, filename):
+    return f"server/{instance.id}/server_banner/{filename}"
 
 
 def category_icon_path(instance, filename):
@@ -67,11 +77,44 @@ class Channel(models.Model):
     # one to one with channel server (one channel can only be in one server) (one server can have many channels)
     server = models.ForeignKey(Server, related_name="channel_server", on_delete=models.CASCADE)
     member = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="channel_member", blank=True)
+    banner = models.ImageField(
+        upload_to=server_banner_path, blank=True, null=True, validators=[validate_image_file_exstension]
+    )
+    icon = models.ImageField(
+        upload_to=server_icon_path,
+        blank=True,
+        null=True,
+        validators=[validate_icon_image_size, validate_image_file_exstension],
+    )
 
-    # override save method to make channel name lowercase and save it
+    # TODO: code repeated from server model need to refactor to utils
+
+    # save method will be called when a category is saved and updated
     def save(self, *args, **kwargs):
+        # if the id already exists, then it is an update
+        if self.id:
+            existing = get_object_or_404(Channel, id=self.id)
+            # if the icon is different, then delete the old icon
+            if existing.icon and self.icon and existing.icon != self.icon:
+                existing.icon.delete(save=False)
+            if existing.banner and self.banner and existing.banner != self.banner:
+                existing.banner.delete(save=False)
         self.name = self.name.lower()
         super(Channel, self).save(*args, **kwargs)
+
+    # delete the icon when the category is deleted
+    @receiver(models.signals.pre_delete, sender="server.Server")
+    def delete_category_icon(sender, instance, **kwargs):
+        # loop through all the fields in the model
+        for field in instance._meta.fields:
+            # if the field is the icon field
+            if field.name == "icon" or field.name == "banner":
+                # get the file
+                file = getattr(instance, field.name)
+                if file:
+                    # delete the file
+                    # save=False --> do not save the model because it is already saved after this
+                    file.delete(save=False)
 
     def __str__(self):
         return self.name
